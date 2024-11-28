6f7aa0302f38523473af33ac393958258a822bb6
Luiz Felipe <felpsgalesso@gmail.com>
2024-11-27 22:17:17 -0300

    Adiciona imagem

--- primeiro-pagina/main.js
+++ primeiro-pagina/main.js
@@ -5,19 +5,8 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <h1> Versionando minha aplicação web</h1>
+    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/243c6e16-ace7-4101-af38-a8aa88d389bb/dfydir4-333f4823-2363-4c26-b736-d7194438200a.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI0M2M2ZTE2LWFjZTctNDEwMS1hZjM4LWE4YWE4OGQzODliYlwvZGZ5ZGlyNC0zMzNmNDgyMy0yMzYzLTRjMjYtYjczNi1kNzE5NDQzODIwMGEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.2xQ7zHQbESypX4lcb45lsRUBbgan-ZGUqsFFzhr0q4U" >
   </div>
 `
 
